1634b4a211a74637bd0d7ea9c51f21515044e8fb
SanjanaBR04 <sanjanabr041204@gmail.com>
2025-08-26 14:20:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const quizRoutes = require("./routes/quiz");
 console.log("✅ quizRoutes loaded:", typeof quizRoutes);
 
 // ✅ MongoDB connection
-mongoose.connect("process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
